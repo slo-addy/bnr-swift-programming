e0e8f56dfcbea804a2c1ea0243681698aaf76c26
Addison Francisco <addisonfrancisco@Addison-iMac.local>
2016-02-22 18:20:10 -0800

    Completed Chapter 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+str += "!"
+print(str)
+
+// Bronze Challenge
+
+var lastName = "Francisco"
+print(lastName)
